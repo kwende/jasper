7ec67442b515b6325c0d8361549f2f732a0440da
root <root@raspberrypi.(none)>
2015-09-05 16:13:37 +0000

    Weather stuff is working.

--- client/modules/WeatherGov.py
+++ client/modules/WeatherGov.py
@@ -1,15 +1,20 @@
 import re
 import urllib2
 import libxml2
+import random
 
 WORDS = ["WEATHER", "TOMORROW"]
 
 def isValid(text):
-	if "WEATHER" in text and ("TOMORROW" in text or "TODAY" in text):
+	if ("WEATHER" in text and ("TOMORROW" in text or "TODAY" in text)) or "TEMPERATURE" in text:
 		return True
 
 def handle(text, mic, profile):
-	mic.say("Give me a second sir, I'm checking online")
+	responses = ["Give me a second to check online", "Checking", "Hold on while I look that up", "One second, please"]
+
+	response = random.choice(responses)
+
+	mic.say(response)
 	response = urllib2.urlopen('http://forecast.weather.gov/MapClick.php?lat=40.7844&lon=-96.655&unit=0&lg=english&FcstType=dwml')
 	data = response.read()
 	doc = libxml2.parseDoc(data)
@@ -19,8 +24,14 @@ def handle(text, mic, profile):
 	if "TODAY" in text:
 		result = xpathStuff.xpathEval('//wordedForecast/text[1]')
 		mic.say(result[0].content)
-	else if "TOMORROW" in text:
+	elif "TOMORROW" in text:
 		result = xpathStuff.xpathEval('//wordedForecast/text[2]')
 		mic.say(result[0].content)
-	else
+	elif "TEMPERATURE" in text:
+		result = xpathStuff.xpathEval("//data[@type='current observations']/parameters/temperature[1]/value")
+		tempValue = result[0].content
+		result = xpathStuff.xpathEval("//data[@type='current observations']/parameters/temperature[2]/value")
+		dewPoint = result[0].content
+		mic.say("The current temperature is " + tempValue + " degrees with a dewpoint of " + dewPoint + " degrees.")
+	else:
 		mic.say("Sorry, all you've taught me so far is about weather today and tomorrow.")
